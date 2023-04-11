012b0cd8cc1d0764519e88dc86784cc967aeb5f0
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2023-04-11 11:54:05 +0200

    Add project files.

---
+++ H2001/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banana banana = new Banana();
+            Apple apple = new Apple();
+            Pineapple pineapple = new Pineapple();
+
+            
+
+            List<int> Fruits = new List<int>();
+
+            
+            Fruits.Add(banana.banana = 2);
+            Fruits.Add(apple.apple = 4);
+            Fruits.Add(pineapple.pineapple = 9);
+
+            Console.WriteLine("Banana = " + Fruits[0]);
+            Console.WriteLine("Apple = " + Fruits[1]);
+            Console.WriteLine("Pineapple = " + Fruits[2]);
+
+            int antal = 0;
+
+            foreach (int line in Fruits) 
+            {
+                antal ++;
+            }
+
+            Console.WriteLine("Summen for alle tre frugter er = "+ 15);
+            Console.WriteLine("Alle frugter lagt sammen 3 gange = " + antal * (Fruits[0] + Fruits[1] + Fruits[2]));
+            Console.ReadKey();
+        }
+        public class Banana
+        {
+            public int banana { get; set; }
+        }
+
+        public class Apple
+        {
+            public int apple { get; set; }
+        }
+
+        public class Pineapple
+        {
+            public int pineapple { get; set; }
+        }
+
+    }
+}
